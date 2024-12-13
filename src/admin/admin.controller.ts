d61bc5c02e2818f2c45e464a370c551f10b26fd2
LyneSultan <lynesultane@gmail.com>
2024-12-13 22:38:09 +0200

    added addCompany endpoint

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AdminService } from './admin.service';
+import { AddCompanyDto } from './DTO/admin.dto';
 
 @Controller('admin')
-export class AdminController{
+export class AdminController {
+  constructor(private readonly adminService: AdminService) {}
 
- }
+  @Post('addCompany')
+  async addCompany(@Body() addCompanyDto: AddCompanyDto): Promise<any> {
+    return this.adminService.addCompany(addCompanyDto);
+  }
+}

3f394c8b0ef00964ff14ebc062530d24fa6edf82
LyneSultan <lynesultane@gmail.com>
2024-12-13 22:37:52 +0200

    added addCompany method

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Company } from '../../schema/company.schema';
+import { AddCompanyDto } from './DTO/admin.dto';
 
 @Injectable()
 export class AdminService {
+  constructor(@InjectModel(Company.name) private companyModel: Model<Company>) {}
 
+  async addCompany(addCompanyDto: AddCompanyDto): Promise<any> {
+    const newCompany = new this.companyModel(addCompanyDto);
+    await newCompany.save();
+    return {
+      message: 'Company created successfully',
+      company: newCompany,
+    };
+  }
 }

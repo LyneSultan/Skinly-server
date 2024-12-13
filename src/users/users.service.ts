78fae7fb0fb40daf1bb920dae5826c405e06e8c6
LyneSultan <lynesultane@gmail.com>
2024-12-13 16:44:04 +0200

    added login method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateUserDto } from './DTO/user.dto';
+import { CreateUserDto, LoginDto } from './DTO/user.dto';
 import { User } from './user.schema';
 
 @Injectable()
@@ -11,5 +11,18 @@ export class UsersService {
   async createUser(createUserDto:CreateUserDto): Promise<User> {
     const newUser = new this.userModel(createUserDto);
     return newUser.save();
+  }
+
+  async login(loginDto: LoginDto): Promise<any>  {
+    const user =await this.userModel.findOne({name:loginDto.name})
+    if (!user) {
+      return {message:"Invalid credentials"}
+    }
+
+    if (user.password !== loginDto.password)
+      return {message:"Invalid credentials"}
+
+    return { message: "login successful", user: user };
+
   }
 }

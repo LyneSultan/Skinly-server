231784f168ea59c7fa99150aaba3dfdeaf9f7e02
LyneSultan <lynesultane@gmail.com>
2024-12-14 00:13:36 +0200

    added method to add product to a certain company

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Company } from '../../schema/company.schema';
 import { User } from '../../schema/user.schema';
+import { AddProductDto } from './DTO/addProduct.dto';
 import { AddCompanyDto } from './DTO/admin.dto';
 
 @Injectable()
@@ -41,4 +42,22 @@ export class AdminService {
       users:users
     }
   }
+  async addProductToCompany(companyId: String, addProductDto: AddProductDto): Promise<any> {
+    const company = await this.companyModel.findById(companyId);
+    if (!company) {
+      return { message: 'Company not found' };
+    }
+
+    const updatedCompany = await this.companyModel.findByIdAndUpdate(
+      companyId,
+      { $push: { products: addProductDto } },
+      { new: true }
+    );
+
+    return {
+      message: 'Product added to company successfully',
+      company: updatedCompany,
+    };
+  }
+
 }

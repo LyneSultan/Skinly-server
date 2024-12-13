f6c6560f66442355609083e91964a000ac87fa28
LyneSultan <lynesultane@gmail.com>
2024-12-13 22:53:37 +0200

    added getCompanies route

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AddCompanyDto } from './DTO/admin.dto';
 
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
+
+  @Get('getCompanies')
+  async getCompanies(): Promise<any>{
+    return this.adminService.getCompanies();
+  }
 
   @Post('addCompany')
   async addCompany(@Body() addCompanyDto: AddCompanyDto): Promise<any> {

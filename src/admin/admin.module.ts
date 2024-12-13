94abfb07f8283b4c5fd07d7b2f0a324d867e0dba
LyneSultan <lynesultane@gmail.com>
2024-12-13 22:38:52 +0200

    updated the adminModule

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Company, CompanySchema } from '../../schema/company.schema';
+import { AdminController } from './admin.controller';
+import { AdminService } from './admin.service';
 
-@Module({})
+@Module({
+  imports: [MongooseModule.forFeature([{ name: Company.name, schema: CompanySchema }])],
+  providers: [AdminService],
+  controllers:[AdminController]
+})
 export class AdminModule {
-
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AdminModule } from './admin/admin.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -7,7 +8,9 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/skinly'),
-    UsersModule],
+    UsersModule,
+    AdminModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

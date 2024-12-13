f401f41e13a77a347ee93996a48a1cfdc53780d1
LyneSultan <lynesultane@gmail.com>
2024-12-13 22:39:11 +0200

    created company schema

---
+++ schema/company.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+interface Product {
+  name: string;
+  price: number;
+  rating: number;
+  description?: string;
+  additional_info: {
+    advertisement: string;
+  };
+}
+
+@Schema()
+export class Company extends Document {
+  @Prop()
+  name: string;
+
+  @Prop()
+  scraping_file: string;
+
+  @Prop({ type: Array })
+  products: Product[];
+}
+
+export const CompanySchema = SchemaFactory.createForClass(Company);

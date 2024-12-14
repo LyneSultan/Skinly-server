6ccd660696c78ae60757d648128eaf4263dafe25
LyneSultan <lynesultane@gmail.com>
2024-12-14 16:04:20 +0200

    added addAds route

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { CompanyService } from './company.service';
+import { AddAdvertisementDto } from './DTO/AddAdvertisement.dto';
 
 @Controller('company')
-export class CompanyController {}
+export class CompanyController {
+  constructor(private readonly companyService: CompanyService) { }
+
+  @Post('addAds/:companyId/:product')
+  async addAds(
+    @Param('companyId') companyId: string,
+    @Param('product') productName: string,
+    @Body() addDto:AddAdvertisementDto): Promise<any> {
+    return  await this.companyService.addAdvertisementToProduct(companyId, productName, addDto);
+  }
+}

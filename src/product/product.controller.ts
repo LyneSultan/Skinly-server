a647180865022942f277465d25f22b3e089b4e59
LyneSultan <lynesultane@gmail.com>
2024-12-18 22:35:20 +0200

    modified the routes

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -6,12 +6,12 @@ import { ProductService } from './product.service';
 export class ProductController {
   constructor(private readonly productService: ProductService) { }
 
-  @Get('getProductsCompany/:id')
+  @Get('/:id')
   async getProductsCompany(@Param('id') companyId:String): Promise<any>{
     return this.productService.getProductsFromCompany(companyId);
   }
 
-  @Post('addProduct/:id')
+  @Post('/:id')
   async addProduct(@Param('id') companyId: String,
     @Body() addProductDto: AddProductDto): Promise<any> {
     return this.productService.addProductToCompany(companyId, addProductDto);
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ProductService } from './product.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Company, CompanySchema } from 'schema/company.schema';
 import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
 
 @Module({
+  imports: [MongooseModule.forFeature([
+      { name: Company.name, schema: CompanySchema }])],
   providers: [ProductService],
   controllers: [ProductController]
 })

6382ef2560acf69fd4f3631202567c74568a646d
LyneSultan <lynesultane@gmail.com>
2024-12-19 21:01:46 +0200

    created get all products method and added pagination for getCompanyProduct

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { paginateData } from 'pagination/pagination';
 import { Company, Product } from 'schema/company.schema';
 import { AddProductDto } from './DTO/addProduct.dto';
 
@@ -25,29 +26,46 @@ export class ProductService {
 
       return updatedCompany;
     } catch (error) {
-      throw new HttpException("Error in adding product", HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException("Error in adding product"+error.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
-  async getProductsFromCompany(comapnyId: string):Promise <Product[]>{
+  async getProductsFromCompany(comapnyId: string,page: number = 2, pageSize: number = 5):Promise<Product[]>{
     try {
       const company = await this.companyModel.findById(comapnyId);
 
       if (!company) {
         throw new HttpException("Company not found", HttpStatus.NOT_FOUND);
       }
+      const paginatedProducts = paginateData(company.products, page, pageSize);
 
-      return company.products;
+      return paginatedProducts;
     } catch (error) {
-      throw new HttpException("Error getting products", HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException("Error getting products "+error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+  async getAllProducts(page: number = 1, pageSize: number = 5) : Promise<Product[]>{
+    try {
+      const companies = await this.companyModel.find().select('products');
+
+      const productsPerCompany = Math.floor(pageSize / companies.length);
+
+      const paginatedCompanies = [];
+
+      for (const company of companies) {
+        const paginatedProducts = paginateData(company.products, page, productsPerCompany);
+        paginatedCompanies.push( paginatedProducts);
+      }
+      return paginatedCompanies;
+
+    } catch (error) {
+      throw new HttpException('Error getting products with pagination'+error.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
-  async getCommonProduct(productName: string): Promise<any> {
+  async getCommonProduct(productName: string) {
     try {
-      const companies = await this.companyModel.find({
-        'products.name': productName,
-      });
+      const companies = await this.companyModel.find({'products.name': productName});
 
       if (!companies || companies.length === 0) {
         throw new HttpException('No companies found with this product', HttpStatus.NOT_FOUND);
@@ -56,9 +74,7 @@ export class ProductService {
       const commonProduct: { product: Product, companyName: string }[] = [];
 
       companies.forEach(company => {
-        const matchingProducts = company.products.filter(
-          product => product.name === productName
-        );
+        const matchingProducts = company.products.filter(product => product.name === productName);
 
         matchingProducts.forEach(product => {
           commonProduct.push({ companyName: company.name, product });
@@ -68,7 +84,7 @@ export class ProductService {
       return commonProduct;
 
     } catch (error) {
-      throw new HttpException('Error finding common products', HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException('Error finding common products'+error.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 

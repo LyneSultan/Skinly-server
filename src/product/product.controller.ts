062680368c96d58ea0ff761d3977494d84486d58
LyneSultan <lynesultane@gmail.com>
2024-12-19 21:02:18 +0200

    added route to get all products

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -6,6 +6,11 @@ import { ProductService } from './product.service';
 export class ProductController {
   constructor(private readonly productService: ProductService) { }
 
+  @Get('')
+  async getAllProducts() {
+    return this.productService.getAllProducts();
+  }
+
   @Get('/:id')
   async getProductsCompany(@Param('id') companyId:string){
     return this.productService.getProductsFromCompany(companyId);

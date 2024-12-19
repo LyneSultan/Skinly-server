93335a5977422c0a3abe15643d5733e873588ded
LyneSultan <lynesultane@gmail.com>
2024-12-19 13:06:08 +0200

    added common product route

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -7,12 +7,17 @@ export class ProductController {
   constructor(private readonly productService: ProductService) { }
 
   @Get('/:id')
-  async getProductsCompany(@Param('id') companyId:String){
+  async getProductsCompany(@Param('id') companyId:string){
     return this.productService.getProductsFromCompany(companyId);
   }
 
+  @Get('common/:productName')
+  async getCommonProducts(@Param('productName') productName: string) {
+    return this.productService.getCommonProduct(productName);
+  }
+
   @Post('/:id')
-  async addProduct(@Param('id') companyId: String,
+  async addProduct(@Param('id') companyId: string,
     @Body() addProductDto: AddProductDto) {
     return this.productService.addProductToCompany(companyId, addProductDto);
   }

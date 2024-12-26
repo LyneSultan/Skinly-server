1921ec157188c5b36d6b2f1c7616e3cb414f1194
LyneSultan <lynesultane@gmail.com>
2024-12-26 21:39:30 +0200

    added login to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'schema/user.schema';
+import { LoginDto } from 'src/users/DTO/userLogin.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly jwtService: JwtService,@InjectModel(User.name) private userModel: Model<User>
-  ) { }
+  constructor(private readonly jwtService: JwtService,
+    @InjectModel(User.name) private userModel: Model<User>) { }
 
+  async login(loginDto: LoginDto) {
+
+    const user = await this.userModel.findOne({ email: loginDto.email, });
+    if (!user) {
+      throw new HttpException('Invalid credentials', HttpStatus.BAD_REQUEST);
+    }
+    if (user.password !== loginDto.password) {
+      throw new HttpException('Invalid credentials', HttpStatus.BAD_REQUEST);
+
+    }
+    const payload = { username: user.name, sub: user.id, role: user.user_type };
+    return {
+      user,
+      access_token: this.jwtService.sign(payload),
+    };
+
+  }
 }

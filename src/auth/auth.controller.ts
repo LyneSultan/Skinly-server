f9e6d7cb62227367f794b906be4ca87999ef9a61
LyneSultan <lynesultane@gmail.com>
2024-12-29 20:06:01 +0200

    added register route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDto } from 'src/users/DTO/userLogin.dto';
+import { CreateUserDto } from 'src/users/DTO/userRegister.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -10,4 +11,10 @@ export class AuthController {
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto){
+    return this.authService.createUser(createUserDto);
+  }
+
 }

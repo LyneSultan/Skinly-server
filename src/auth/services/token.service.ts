dbe3e67e8e3cf526f4fab37577051d6ccd6e4e93
LyneSultan <lynesultane@gmail.com>
2025-01-05 14:04:27 +0200

    updated token service

--- src/auth/services/token.service.ts
+++ src/auth/services/token.service.ts
@@ -3,7 +3,6 @@ import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class TokenInterceptor implements NestInterceptor {
-  private secretKey = 'your_secret_key';
 
   intercept(context: ExecutionContext, next: CallHandler) {
     const request = context.switchToHttp().getRequest();
@@ -11,7 +10,7 @@ export class TokenInterceptor implements NestInterceptor {
 
     if (token) {
       try {
-        const decoded = jwt.verify(token, this.secretKey);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
         request.user = decoded;
       } catch (error) {
         throw new HttpException('Token expired or invalid', HttpStatus.BAD_REQUEST);

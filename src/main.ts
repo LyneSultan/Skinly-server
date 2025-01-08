3938f85da88d76847e44877c3f374646437a4e89
LyneSultan <lynesultane@gmail.com>
2025-01-08 21:20:37 +0200

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: false,
     transform: true,
   }));
-
+  app.enableCors({
+    origin:process.env.FRONT_URL  ,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

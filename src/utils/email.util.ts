5954169b26f98ab07b5482d0c652b80e4f43ad0c
LyneSultan <lynesultane@gmail.com>
2025-01-14 15:44:37 +0200

    updated the sender email

--- src/utils/email.util.ts
+++ src/utils/email.util.ts
@@ -1,13 +1,17 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 import axios from 'axios';
 
-export const sendEmailWithPassword = async (name: string, toEmail: string,subject:string,htmlPart:string) =>
-{
+export const sendEmailWithPassword = async (
+  name: string,
+  toEmail: string,
+  subject: string,
+  htmlPart: string,
+) => {
   const emailBody = {
     Messages: [
       {
         From: {
-          Email: 'lynesultane@gmail.com',
+          Email: 'skinly.personal.advisor@gmail.com',
           Name: 'Skinly',
         },
         To: [
@@ -23,15 +27,18 @@ export const sendEmailWithPassword = async (name: string, toEmail: string,subjec
   };
 
   try {
-    const response = await axios.post('https://api.mailjet.com/v3.1/send', emailBody, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Basic ${Buffer.from(`${process.env.API_KEY_EMAIl}:${process.env.KEY}`).toString('base64')}`,
+    const response = await axios.post(
+      'https://api.mailjet.com/v3.1/send',
+      emailBody,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${Buffer.from(`${process.env.API_KEY_EMAIl}:${process.env.KEY}`).toString('base64')}`,
+        },
       },
-    });
+    );
     return response.data;
   } catch (error) {
-    console.error('Error sending email:', error);
-    throw new HttpException('Failed to send email', HttpStatus.INTERNAL_SERVER_ERROR);
+    throw new HttpException( 'Failed to send email', HttpStatus.INTERNAL_SERVER_ERROR );
   }
-}
+};

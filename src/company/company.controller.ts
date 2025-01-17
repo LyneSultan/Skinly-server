2a80b55a994797e74e22a03ee702abe68f386350
LyneSultan <lynesultane@gmail.com>
2025-01-17 18:43:13 +0200

    updated company module

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,4 +1,17 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Request, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Request,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { Company } from 'schema/company.schema';
 import { AdminGuard } from 'src/auth/admin.guard';
 import { TokenInterceptor } from 'src/auth/services/token.service';
@@ -7,29 +20,37 @@ import { addCompany } from './DTO/addCompany.dto';
 
 @Controller('company')
 export class CompanyController {
-  constructor(private readonly companyService: CompanyService) { }
+  constructor(private readonly companyService: CompanyService) {}
 
   @Get('/')
-  async getCompanies(){
+  @UseGuards(AdminGuard)
+  async getCompanies() {
+
     return this.companyService.getCompanies();
   }
 
   @Post('/')
   @UseGuards(AdminGuard)
-  async addCompany(@Body() addCompanyDto: addCompany) {
-    return this.companyService.addCompany(addCompanyDto);
+  @UseInterceptors(FileInterceptor('file'))
+  async addCompany(@Body() addCompanyDto: addCompany, @UploadedFile() file: Express.Multer.File) {
+
+    return this.companyService.addCompany(addCompanyDto, file);
   }
 
   @Delete('/:id')
   @UseGuards(AdminGuard)
-  async removeCompany(@Param('id')companyId:String){
+  async removeCompany(@Param('id') companyId: String) {
+
     return this.companyService.removeCompany(companyId);
   }
 
   @Patch()
   @UseInterceptors(TokenInterceptor)
-  async updateUser(@Request() req,@Body() updateData: Partial<Company>) {
-      const companyId = req.user.sub;
-      return this.companyService.updateCompany(companyId, updateData);
+  async updateUser(@Request() req, @Body() updateData: Partial<Company>) {
+
+    const companyId = req.user.sub;
+    return this.companyService.updateCompany(companyId, updateData);
   }
+
+
 }
--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -3,14 +3,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Company, CompanySchema } from 'schema/company.schema';
 import { User, UserSchema } from 'schema/user.schema';
 import { AuthModule } from 'src/auth/auth.module';
+import { ScraperService } from 'src/scraper/scraper.service';
 import { CompanyController } from './company.controller';
 import { CompanyService } from './company.service';
 
 @Module({
-
-  imports: [MongooseModule.forFeature([
-  { name: Company.name, schema: CompanySchema },{ name: User.name, schema: UserSchema }]),AuthModule],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Company.name, schema: CompanySchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+    AuthModule,
+  ],
   controllers: [CompanyController],
-  providers: [CompanyService]
+  providers: [CompanyService, ScraperService],
 })
 export class CompanyModule {}

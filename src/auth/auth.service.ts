9dac3911a4659176f7f6a1a2fcbc8379e811d7c3
LyneSultan <lynesultane@gmail.com>
2025-01-17 18:43:51 +0200

    updated auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,93 +9,124 @@ import { sendEmailWithPassword } from 'src/utils/email.util';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService,
-    @InjectModel(User.name) private userModel: Model<User>) { }
+  constructor(
+    private readonly jwtService: JwtService,
+    @InjectModel(User.name) private userModel: Model<User>
+  ) {}
 
   async login(loginDto: LoginDto) {
     try {
-      const user = await this.userModel.findOne({ email: loginDto.email, });
+      const user = await this.userModel.findOne({ email: loginDto.email });
+
       if (!user) {
-        throw new HttpException( { message: ['Invalid credentials'] }, HttpStatus.BAD_REQUEST);
+        throw new HttpException({ message: ['Invalid credential'] }, HttpStatus.BAD_REQUEST);
       }
+
+      // const isPasswordValid = await bcrypt.compare(loginDto.password, user.password);
+
       if (user.password !== loginDto.password) {
-        throw new HttpException( { message: ['Invalid credentials'] }, HttpStatus.BAD_REQUEST);
-
+        throw new HttpException( { message: ['Invalid credentials'] },HttpStatus.BAD_REQUEST );
       }
+
       if (user.ban) {
-        throw new HttpException( { message: ['banned'] }, HttpStatus.FORBIDDEN);
-
+        throw new HttpException({ message: ['banned'] }, HttpStatus.FORBIDDEN);
       }
-      const payload = { username: user.name, sub: user.id, role: user.user_type };
+      const payload = {
+        username: user.name,
+        sub: user.id,
+        role: user.user_type,
+      };
+
       return {
         user,
         access_token: this.jwtService.sign(payload),
       };
-    }catch (error) {
+
+    } catch (error) {
+
       if (error instanceof HttpException) {
         throw error;
       }
-      throw new HttpException({ message: ['An unexpected error occurred'] }, HttpStatus.INTERNAL_SERVER_ERROR,);
+
+      throw new HttpException({ message: ['An unexpected error occurred'] },HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
   async createUser(createUserDto: CreateUserDto) {
     try {
-      const existingEmail = await this.userModel.findOne({ 'email': createUserDto.email });
+      const existingEmail = await this.userModel.findOne({ email: createUserDto.email,});
 
       if (existingEmail) {
-        throw new HttpException({ message: ['Choose another email'] }, HttpStatus.BAD_REQUEST);
+        throw new HttpException( { message: ['Choose another email'] }, HttpStatus.BAD_REQUEST );
       }
 
       if (createUserDto.password !== createUserDto.password_confirmation) {
-        throw new HttpException({ message: ['Passwords do not match'] }, HttpStatus.BAD_REQUEST);
+        throw new HttpException({ message: ['Passwords do not match'] }, HttpStatus.BAD_REQUEST );
       }
+
       const newUser = new this.userModel(createUserDto);
+      // const hashedPassword = await bcrypt.hash(newUser.password, 10);
+      // newUser.password = hashedPassword;
       await newUser.save();
-      const payload = { username: newUser.name, sub: newUser.id, role: newUser.user_type };
+
+      const payload = {
+        username: newUser.name,
+        sub: newUser.id,
+        role: newUser.user_type,
+      };
+
       return {
-        newUser,
+        user: newUser,
         access_token: this.jwtService.sign(payload),
       };
     } catch (error) {
+
       if (error instanceof HttpException) {
         throw error;
       }
-      throw new HttpException({ message: ['An unexpected error occurred'+error]},HttpStatus.INTERNAL_SERVER_ERROR,);}
+
+      throw new HttpException({ message: ['An unexpected error occurred' + error] }, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-
-  async resetpassword(email: string,password:string){
-    const user = await this.userModel.findOne({ email: email, });
+  async resetpassword(email: string, password: string) {
+    const user = await this.userModel.findOne({ email: email });
 
     if (!user) {
-      throw new HttpException('Email not found in the database', HttpStatus.NOT_FOUND);
+      throw new HttpException('Email not found in the database', HttpStatus.NOT_FOUND );
     }
 
     user.password = password;
     await user.save();
+
     const payload = { username: user.name, sub: user.id, role: user.user_type };
     return {
       user,
       access_token: this.jwtService.sign(payload),
     };
-
   }
-  async sendVerificationCode(email: string){
-    const user = await this.userModel.findOne({ email: email });
 
-    if (!user) {
-      throw new HttpException('Email not found in the database', HttpStatus.NOT_FOUND);
+  async sendVerificationCode(email: string) {
+    try {
+
+
+      const user = await this.userModel.findOne({ email: email });
+
+      if (!user) {
+        throw new HttpException('Email not found in the database', HttpStatus.NOT_FOUND);
+      }
+
+      const verificationCode = Math.floor(1000 + Math.random() * 9000).toString();
+
+      const subject = 'Verification code';
+      const emailContent = `<h3>Hello,</h3><p> Your Verifcation code is
+       <strong>${verificationCode}</strong></p>`;
+
+      await sendEmailWithPassword(user.name, user.email, subject, emailContent);
+
+      return verificationCode;
+    } catch (error) {
+      throw new HttpException({ message: ['An unexpected error occurred' + error] }, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-    const verificationCode = Math.floor(1000 + Math.random() * 9000).toString();
-
-    const subject = "Verification code";
-    const emailContent=`<h3>Hello,</h3><p>Your account has been created. Your login password is: <strong>${verificationCode}</strong></p>`
-
-    await sendEmailWithPassword(user.name, user.email, subject,emailContent);
-
-    return verificationCode;
   }
-
 }

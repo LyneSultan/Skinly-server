fc006f48bea53dbfb2ba40829cb04b21b1d40b78
LyneSultan <lynesultane@gmail.com>
2025-01-19 22:45:07 +0200

    updated product controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,20 +1,38 @@
-import { Body, Controller, Get, Param, Post, Query, Request, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Request,
+  UseInterceptors,
+} from '@nestjs/common';
 import { TokenInterceptor } from 'src/auth/services/token.service';
 import { AddProductDto } from './DTO/addProduct.dto';
 import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
-  constructor(private readonly productService: ProductService) { }
+  constructor(private readonly productService: ProductService) {}
 
   @Get('')
-  async getAllProducts(@Query('page') page: number= 3,@Query('pageSize') pageSize: number= 5) {
-    return this.productService.getAllProducts(page,pageSize);
+  async getAllProducts( @Query('page') page: number = 3, @Query('pageSize') pageSize: number = 5) {
+
+    return this.productService.getAllProducts(page, pageSize);
   }
+
   @Get('company')
   @UseInterceptors(TokenInterceptor)
-  async getProductsCompany(@Request()req, @Query('page') page: number = 3, @Query('pageSize') pageSize: number = 5) {
-    return this.productService.getProductsFromCompany(req.user.sub,page,pageSize);
+  async getProductsCompany( @Request() req, @Query('page') page: number = 3, @Query('pageSize') pageSize: number = 5) {
+
+    return this.productService.getProductsFromCompany( req.user.sub,page,pageSize );
+  }
+
+  @Post('search')
+  @UseInterceptors(TokenInterceptor)
+  async searchProductCompany(@Request() req, @Body('name') name: string) {
+    return this.productService.searchProductInCompany(req.user.sub, name);
   }
 
   @Get('common/:productName')
@@ -23,8 +41,20 @@ export class ProductController {
   }
 
   @Post('/:id')
-  async addProduct(@Param('id') companyId: string,
-    @Body() addProductDto: AddProductDto) {
+  async addProduct(  @Param('id') companyId: string,@Body() addProductDto: AddProductDto) {
     return this.productService.addProductToCompany(companyId, addProductDto);
+  }
+
+  @Get('/skin/:skinType')
+  async getRecommendation(@Param('skinType') skinType: string) {
+    return this.productService.suggestBestProducts(skinType);
+  }
+  @Get('/skin')
+  async getRecommendationq() {
+    return this.productService.suggestProducts();
+  }
+  @Get('/sample')
+  async getRandom() {
+    return this.productService.getAllRandomProducts();
   }
 }

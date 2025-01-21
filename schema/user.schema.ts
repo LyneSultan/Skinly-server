359dd233c45f13573f9282969baeabcc06b6b3dc
LyneSultan <lynesultane@gmail.com>
2025-01-21 18:46:02 +0200

    updated user schema

--- schema/user.schema.ts
+++ schema/user.schema.ts
@@ -7,7 +7,7 @@ export class User extends Document{
   @Prop({ required: true })
   name: string;
 
-  @Prop({ required: true })
+  @Prop({ required: true,select:false })
   password: string;
 
   @Prop({ required: true , unique:true, match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/})
